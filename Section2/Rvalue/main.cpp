ea12934503a469d5fac8bfd7f1bd1d9e6f73c813
wgaonar <wgaonar@gmail.com>
2023-06-08 05:51:59 -0600

    Section - Part 3 completed

---
+++ Section2/Rvalue/main.cpp
@@ -0,0 +1,15 @@
+// Example of a function argument which is an rvalue reference
+#include <iostream>
+
+// The caller's object will be moved into a x
+void func(int&& x)
+{
+  std::cout << "Rvalue reference" << std::endl;
+}
+int main()
+{
+  func(2);
+
+  int y{2};
+  // func(y); // Error! Must be an rvalue moveable object
+}
