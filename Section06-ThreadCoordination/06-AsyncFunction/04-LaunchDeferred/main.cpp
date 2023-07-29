d9507bea4922dd690c86c4d64cc07eb71b63f9bc
wgaonar <wgaonar@gmail.com>
2023-07-28 19:12:46 -0600

    Folders and files organized

---
+++ Section06-ThreadCoordination/06-AsyncFunction/04-LaunchDeferred/main.cpp
@@ -0,0 +1,35 @@
+/* 
+  Example of using std::async with deferred launch option
+*/
+
+#include <iostream>
+#include <future>
+
+using namespace std::literals;
+
+// Callable object - thread entry point
+int func ()
+{
+  std::cout << "Executing in func from thread with ID: "
+            << std::this_thread::get_id() << "\n";
+  std::this_thread::sleep_for(5s);
+
+  std::cout << "After 7s (not 5s because is the same thread!), returning \"42\" from func" << "\n";
+  return 42;
+}
+int main()
+{
+  std::cout << "In main thread with ID " 
+            << std::this_thread::get_id() << "\n";
+  std::cout << "Calling func" << "\n";
+
+  // Launch the task          
+  auto result = std::async(std::launch::deferred, func);
+  
+  // Sleep the main thread
+  std::this_thread::sleep_for(2s);
+
+  // Calling the results
+  std::cout << "After 2s, calling get()" << "\n";
+  std::cout << result.get() << "\n";
+}
